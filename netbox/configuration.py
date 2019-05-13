fc5f65ae8b33af39e0e68d6df8c05966fe77cc08
tom_vandevelde <tom_vandevelde@be.ibm.com>
2019-05-13 14:49:00 +0200

    Allow everybody

--- netbox/configuration.py
+++ netbox/configuration.py
@@ -8,7 +8,7 @@
 # access to the server via any other hostnames. The first FQDN in the list will be treated as the preferred name.
 #
 # Example: ALLOWED_HOSTS = ['netbox.example.com', 'netbox.internal.local']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # PostgreSQL database configuration.
 DATABASE = {
